33431c79bb2bee3269c590b9ee700394b8dcaeb5
Tyler Pelaez <mail@tylerpelaez.com>
2023-06-16 13:23:07 -0400

    Polish

---
+++ MusicSound/SFXController.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@onready var one_off_prefab = preload("res://Scenes/AudioEffect.tscn")
+
+@onready var trap_sfx = $TrapSFX
+@onready var enemy_sfx = $EnemySFX
+
+func play_trap_sfx(stream: AudioStream):
+	play_audio_in_group(trap_sfx, stream)
+	
+func play_audio_in_group(group: Node2D, stream: AudioStream):
+	for child in group.get_children():
+		if !(child is AudioStreamPlayer):
+			continue
+		
+		if !child.playing:
+			child.stream = stream
+			child.play()
+
+func play_enemy_sfx(stream: AudioStream):
+	play_audio_in_group(enemy_sfx, stream)
+
+func play_one_off(stream: AudioStream):
+	var fx = one_off_prefab.instantiate()
+	get_tree().root.add_child(fx)
+	fx.play_sound(stream)
--- Player/Traps/Trap.gd
+++ Player/Traps/Trap.gd
@@ -13,10 +13,8 @@ signal on_death
 @onready var shape_cast: ShapeCast2D = $ShapeCast2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-@onready var audio_effect_prefab = preload("res://Scenes/AudioEffect.tscn")
 @onready var destroyed_sfx = preload("res://MusicSound/Trap_Destroyed_SFX.mp3")
 @onready var deployed_sfx = preload("res://MusicSound/Trap_Placement_SFX.mp3")
-@onready var audio_player: AudioStreamPlayer = $AudioStreamPlayer
 
 @export var attack_sfx: AudioStream
 
@@ -24,9 +22,7 @@ var deployed = false
 var is_dying: bool = false
 
 func _ready():
-	audio_player.stop()
-	audio_player.stream = deployed_sfx
-	audio_player.play()
+	SfxController.play_one_off(deployed_sfx)
 	animation_player.play("Deploy")
 
 var is_attacking := false
@@ -45,9 +41,7 @@ func _process(delta):
 func attack() -> void:
 	is_attacking = true
 	if attack_sfx != null:
-		audio_player.stop()
-		audio_player.stream = attack_sfx
-		audio_player.play()
+		SfxController.play_trap_sfx(attack_sfx)
 
 func should_attack() -> bool:
 	return !is_attacking && attack_cooldown_timer.time_left <= 0
@@ -58,12 +52,9 @@ func on_attack_finished():
 
 func _on_stats_no_health():
 	is_dying = true
-	var fx = audio_effect_prefab.instantiate()
-	get_tree().root.add_child(fx)
-	fx.play_sound(destroyed_sfx)
+	SfxController.play_one_off(destroyed_sfx)
 	queue_free()
 	on_death.emit()
-
 
 func _on_hurtbox_area_entered(area):
 	if area is Hitbox:
